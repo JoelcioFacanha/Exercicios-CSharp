9329b9c472ee1a8902fcedd3a7e264ab8b799a14
Joelcio Facanha <joelciomoreira@gmail.com>
2020-07-07 12:29:44 -0300

    Exercicio Concluido

--- Secao04/ControleEstoque/ControleEstoque/Program.cs
+++ Secao04/ControleEstoque/ControleEstoque/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ControleEstoque.Entities;
+using System;
+using System.Globalization;
 
 namespace ControleEstoque
 {
@@ -6,7 +8,32 @@ namespace ControleEstoque
     {
         static void Main(string[] args)
         {
-            
+            Produto produto = new Produto();
+
+            Console.WriteLine("Entre com os dados do Produto:");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade em estoque: ");
+            produto.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + produto);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a serem adicionados ao estoque: ");
+            produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizado: " + produto);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a serem removidos do estoque: ");
+            produto.RemoverProdutos(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizado: " + produto);
         }
     }
 }
