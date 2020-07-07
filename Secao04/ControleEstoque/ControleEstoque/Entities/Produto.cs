35a0693b3a7e6d640a8979ca4a4365596a4ca298
Joelcio Facanha <joelciomoreira@gmail.com>
2020-07-07 12:28:14 -0300

    Criando a classe produto

---
+++ Secao04/ControleEstoque/ControleEstoque/Entities/Produto.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace ControleEstoque.Entities
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", R$ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", " + Quantidade + " unidade(s), Total R$ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
