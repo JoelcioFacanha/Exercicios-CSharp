97d19529cd5b25282a1ed2f8c766f3866e63273d
Joelcio Facanha <joelciomoreira@gmail.com>
2020-07-07 16:34:03 -0300

    Concluindo exercicio proposto 01

---
+++ Secao04/ExercicioProposto01/ExercicioProposto01/Entities/Pessoa.cs
@@ -0,0 +1,8 @@
+﻿namespace ExercicioProposto01.Entities
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+    }
+}
--- Secao04/ExercicioProposto01/ExercicioProposto01/Program.cs
+++ Secao04/ExercicioProposto01/ExercicioProposto01/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExercicioProposto01.Entities;
+using System;
 
 namespace ExercicioProposto01
 {
@@ -6,7 +7,31 @@ namespace ExercicioProposto01
     {
         static void Main(string[] args)
         {
-            
+            Pessoa p1, p2;
+
+            p1 = new Pessoa();
+            p2 = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            p2.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            if (p1.Idade > p2.Idade)
+            {
+                Console.WriteLine("Pessoa mais velha: " + p1.Nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: " + p2.Nome);
+            }
         }
     }
 }
