c64cfa9aed236f02390effd71536a31ddc7fe7ac
Joelcio Facanha <joelciomoreira@gmail.com>
2020-07-07 16:56:45 -0300

    Concluindo o exercicio de fixação 01

--- Secao06/ExercicioFixao01/ExercicioFixao01/Program.cs
+++ Secao06/ExercicioFixao01/ExercicioFixao01/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ExercicioFixacao01.Entities;
+using System;
+using System.Globalization;
 
 namespace ExercicioFixao01
 {
@@ -6,7 +8,36 @@ namespace ExercicioFixao01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pencionato p = new Pencionato();
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Alugel #" + (i + 1));
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Nº do Quarto: ");
+                int numero = int.Parse(Console.ReadLine());
+
+                p.Alugar(new Estudante { Nome = nome, Email = email }, numero);
+
+                Console.WriteLine("");
+            }
+
+            int totalDeQuartos = p.TotalDeQuartos();
+
+            Console.WriteLine("Quartos Ocupados:");
+
+            for (int i = 0; i < totalDeQuartos; i++)
+            {
+                p.ImprimirQuartosOcupados(i);
+            }
         }
     }
 }
