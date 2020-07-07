b378df6b880d4ada1828a2b64b966fbb257daa8d
Joelcio Facanha <joelciomoreira@gmail.com>
2020-07-07 16:42:18 -0300

    Concluindo exercicio extra

--- Secao04/LerCotacaoDolar/LerCotacaoDolar/Program.cs
+++ Secao04/LerCotacaoDolar/LerCotacaoDolar/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LerCotacaoDolar.Entities;
+using System;
+using System.Globalization;
 
 namespace LerCotacaoDolar
 {
@@ -6,7 +8,11 @@ namespace LerCotacaoDolar
     {
         static void Main(string[] args)
         {
-
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Valor a ser pago em reais = " + ConversorDeMoeda.DolarParaReal(cotacao, quantia).ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
