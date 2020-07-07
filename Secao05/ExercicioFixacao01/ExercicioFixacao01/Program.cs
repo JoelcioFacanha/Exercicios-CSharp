804f8981fbc0dcd3b63b2cc059abfb92e0f7ed1a
Joelcio Facanha <joelciomoreira@gmail.com>
2020-07-07 16:48:41 -0300

    Concluindo exercicio de fixacao 01

--- Secao05/ExercicioFixacao01/ExercicioFixacao01/Program.cs
+++ Secao05/ExercicioFixacao01/ExercicioFixacao01/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ExercicioFixacao01.Entities;
+using System;
+using System.Globalization;
 
 namespace ExercicioFixacao01
 {
@@ -6,7 +8,52 @@ namespace ExercicioFixacao01
     {
         static void Main(string[] args)
         {
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char haveraDeposito = char.Parse(Console.ReadLine());
 
+            Conta c;
+            double valor = 0.0;
+
+            if (haveraDeposito.Equals('S'))
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                c = new Conta(numero, titular, valor);
+            }
+            else
+            {
+                c = new Conta(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(c);
+
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para depósito: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Depositar(valor);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(c);
+
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para saque: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Sacar(valor);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(c);
         }
     }
 }
