ab9fac9631a0604d09d6317f5a2541a7aca3af0e
Joelcio Facanha <joelciomoreira@gmail.com>
2020-07-07 16:29:50 -0300

    Exercicio de Fixação 03 concluido

--- Secao04/ExercicioFixacao03/ExercicioFixacao03/Program.cs
+++ Secao04/ExercicioFixacao03/ExercicioFixacao03/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ExercicioFixacao03.Entities;
+using System;
+using System.Globalization;
 
 namespace ExercicioFixacao03
 {
@@ -6,7 +8,21 @@ namespace ExercicioFixacao03
     {
         static void Main(string[] args)
         {
+            Aluno a = new Aluno();
 
+            Console.Write("Nome: ");
+            a.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            Console.Write("Nota 1: ");
+            a.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Nota 2: ");
+            a.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Nota 3: ");
+            a.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+
+            Console.WriteLine(a);
         }
     }
 }
