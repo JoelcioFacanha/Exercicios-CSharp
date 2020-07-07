4a136d68b2cb5d96f162d36ce10c333d9d508a25
Joelcio Facanha <joelciomoreira@gmail.com>
2020-07-07 17:02:09 -0300

    Criando a classe employee

---
+++ Secao06/ExercicioFixacao02/ExercicioFixacao02/Entities/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace ExercicioFixacao02.Entities
+{
+    public class Employee
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public double Salary { get; private set; }
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void AumentarSalario(double percentual)
+        {
+            Salary += (Salary * percentual) / 100;
+        }
+
+        public override string ToString()
+        {
+            return Id + ", " + Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
