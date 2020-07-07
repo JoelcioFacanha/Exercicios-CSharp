64c7af2777b6c9d04e407617bdd14d7cc6504e7c
Joelcio Facanha <joelciomoreira@gmail.com>
2020-07-07 16:38:01 -0300

    Concluindo exercicio proposto 02

---
+++ Secao04/ExercicioProposto02/ExercicioProposto02/Entities/Funcionario.cs
@@ -0,0 +1,8 @@
+﻿namespace ExercicioProposto02.Entities
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+    }
+}
--- Secao04/ExercicioProposto02/ExercicioProposto02/Program.cs
+++ Secao04/ExercicioProposto02/ExercicioProposto02/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ExercicioProposto02.Entities;
+using System;
+using System.Globalization;
 
 namespace ExercicioProposto02
 {
@@ -6,7 +8,26 @@ namespace ExercicioProposto02
     {
         static void Main(string[] args)
         {
+            Funcionario f1, f2;
 
+            f1 = new Funcionario();
+            f2 = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            f1.Nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            f1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            f2.Nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            f2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double mediaSalarial = (f1.Salario + f2.Salario) / 2.0;
+
+            Console.WriteLine("Sálario médio = " + mediaSalarial.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
