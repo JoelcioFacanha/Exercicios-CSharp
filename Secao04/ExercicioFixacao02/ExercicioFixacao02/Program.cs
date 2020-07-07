8803525042f5482949c150079ea336bfc9d98529
Joelcio Facanha <joelciomoreira@gmail.com>
2020-07-07 16:24:49 -0300

    Exercicio Concluido

--- Secao04/ExercicioFixacao02/ExercicioFixacao02/Program.cs
+++ Secao04/ExercicioFixacao02/ExercicioFixacao02/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ExercicioFixacao02.Entities;
+using System;
+using System.Globalization;
 
 namespace ExercicioFixacao02
 {
@@ -6,7 +8,24 @@ namespace ExercicioFixacao02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Funcionario f = new Funcionario();
+            Console.WriteLine("Entre com os dados do funcionário:");
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionário: " + f);
+
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            f.AumentarSalario(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizado: " + f);
         }
     }
 }
