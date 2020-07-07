7696f57a50b8c1b6446391143c347fdbc27321bf
Joelcio Facanha <joelciomoreira@gmail.com>
2020-07-07 16:18:48 -0300

    Criando a classe Retangulo

---
+++ Secao04/ExercicioFixacao01/ExercicioFixacao01/Entities/Retangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExercicioFixacao01.Entities
+{
+    public class Retangulo
+    {
+        public double Largura { get; set; }
+        public double Altura { get; set; }
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Largura, 2) + Math.Pow(Altura, 2));
+        }
+    }
+}
