a2659c6ae688f8a5a3ad91e2c7897ab6f289abee
Joelcio Facanha <joelciomoreira@gmail.com>
2020-07-07 16:20:28 -0300

    Exercicio concluido

--- Secao04/ExercicioFixacao01/ExercicioFixacao01/Program.cs
+++ Secao04/ExercicioFixacao01/ExercicioFixacao01/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ExercicioFixacao01.Entities;
+using System;
+using System.Globalization;
 
 namespace ExercicioFixacao01
 {
@@ -6,7 +8,15 @@ namespace ExercicioFixacao01
     {
         static void Main(string[] args)
         {
+            Retangulo r = new Retangulo();
 
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Area: " + r.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro: " + r.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal: " + r.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
